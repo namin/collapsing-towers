b306ceb5e65dc24087434f5dbc8fac19cdea335c
Nada Amin <namin@alum.mit.edu>
2016-03-09 11:14:13 +0100

    unstaged more complete matcher working

--- matches.scala
+++ matches.scala
@@ -11,7 +11,6 @@ val matches_poly_src = """(let matches (lambda matches r (lambda _ s
   (lambda _ r (maybe-lift (lambda _ s ((matches r) s)))))
 """
 
-
 val matches_src = matches_poly_src.replace("maybe-lift", "nolift")
 val matchesc_src = matches_poly_src.replace("maybe-lift", "lift")
 
@@ -30,6 +29,76 @@ val matchesc_ab_src = matches_ab_poly_src.replace("maybe-lift", "lift")
 
 val Success(matches_ab_val, _) = parseAll(exp, matches_ab_src)
 val Success(matchesc_ab_val, _) = parseAll(exp, matchesc_ab_src)
+
+val matches_bis_src = """
+(let match_loop (lambda match_loop m (lambda _ s
+  (if (equs 'yes (m s))
+      'yes
+      (if (equs 'done (car s))
+          'no
+         ((match_loop m) (cdr s))))))
+(let star_loop (lambda star_loop m (lambda _ c (lambda _ s
+  (if (equs 'yes (m s))
+      'yes
+      (if (equs 'done (car s))
+          'no
+          (if (equs '_ c)
+              (((star_loop m) c) (cdr s))
+              (if (equs c (car s))
+                 (((star_loop m) c) (cdr s))
+                 'no)))))))
+(let match_here (lambda match_here r
+  (if (equs 'done (car r)) (lambda _ s 'yes)
+  (let m (if (equs '_ (car r))
+               (lambda _ s (if (equs 'done (car s)) 'no ((match_here (cdr r)) (cdr s))))
+               (lambda _ s (if (equs 'done (car s)) 'no
+                          (if (equs (car r) (car s))
+                              ((match_here (cdr r)) (cdr s))
+                              'no))))
+    (if (equs 'done (car (cdr r)))
+        (if (equs '$ (car r))
+            (lambda _ s (if (equs 'done (car s)) 'yes 'no))
+            m)
+    (if (equs '* (car (cdr r)))
+        ((star_loop (match_here (cdr (cdr r)))) (car r))
+        m)))))
+(let match (lambda match r
+  (if (equs 'done (car r)) (lambda _ s 'yes)
+      (if (equs '^ (car r))
+          (match_here (cdr r))
+          (match_loop (match_here r)))))
+match))))
+"""
+
+val Success(matches_bis_val, _) = parseAll(exp, matches_bis_src)
+
+val Success(a__val, _) = parseAll(exp, """(^ a _ $ done)""")
+
+val Success(a__star_a_val, _) = parseAll(exp, """(a _ * a done)""")
+
+val Success(a_bstar_a_val, _) = parseAll(exp, """(a b * a done)""")
+
+val Success(abba_val, _) = parseAll(exp, """(a b b a done)""")
+
+val Success(abca_val, _) = parseAll(exp, """(a b c a done)""")
+
+def testMatchesBis() = {
+  def test1(re: Val, s: Val, e: Boolean) = {
+    check(evalms(List(matches_bis_val, re, s),
+      App(App(App(App(eval_exp,Var(0)),Sym("nil-env")),Var(1)), Var(2))
+    ))(if (e) "Str(yes)" else "Str(no)")
+  }
+  test1(ab_val, ab_val, true)
+  test1(ab_val, ac_val, false)
+  test1(ab_val, abba_val, true)
+  test1(a__val, ab_val, true)
+  test1(a__val, ac_val, true)
+  test1(a__val, abba_val, false)
+  test1(a_bstar_a_val, abba_val, true)
+  test1(a_bstar_a_val, abca_val, false)
+  test1(a__star_a_val, abba_val, true)
+  test1(a__star_a_val, abca_val, true)
+}
 
 def testMatches() = {
 println("// ------- test matches --------")
