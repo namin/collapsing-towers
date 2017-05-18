1c2d3b775806f2e154a916b7b70ebd5a1eab4edc
Nada Amin <amin@st.informatik.tu-darmstadt.de>
2017-05-18 17:03:19 +0200

    seems to work, but yuck

--- vm.rkt
+++ vm.rkt
@@ -7,7 +7,8 @@
   (v (lit number) (lam x e) (cons v v) (code e))
   (b plus minus times)
   (E hole (cons E e) (cons v E) (let x E g) (if E e e) (b E e) (b v E) (fix E e) (lift E) (run E e) (reflect E))
-  (R hole (lift (lam x R)) (if (code e) R g) (if (code e) v R) (run (code e) R))
+  (R hole (reify R) (lift (lam x R)) (if (code e) R g) (if (code e) v R) (run (code e) R))
+  (M (reify E) E)
   (x (variable-except lit lam cons let app if plus minus times fix lift run reflect reify pair code)))
 
 (define not-code? (lambda (x) (not ((redex-match vm (code e)) x))))
@@ -17,49 +18,49 @@
 (define red
   (reduction-relation
    vm
-   (--> (in-hole E (let x v e))                 (in-hole E (subst x v e)) "let")
-   (--> (in-hole E (app (lam x e) v))           (in-hole E (subst x v e)) "lam")
-   (--> (in-hole E (if (lit 0) e_1 e_2))        (in-hole E e_2)           "if0")
-   (--> (in-hole E (if (lit number_1) e_1 e_2)) (in-hole E e_1)           "ifn"
+   (--> (in-hole M (let x v e))                 (in-hole M (subst x v e)) "let")
+   (--> (in-hole M (app (lam x e) v))           (in-hole M (subst x v e)) "lam")
+   (--> (in-hole M (if (lit 0) e_1 e_2))        (in-hole M e_2)           "if0")
+   (--> (in-hole M (if (lit number_1) e_1 e_2)) (in-hole M e_1)           "ifn"
         (side-condition (not (= 0 (term number_1)))))
-   (--> (in-hole E (plus (lit number_1) (lit number_2)))
-        (in-hole E (lit ,(+ (term number_1) (term number_2))))             "plus")
-   (--> (in-hole E (minus (lit number_1) (lit number_2)))
-        (in-hole E (lit ,(- (term number_1) (term number_2))))             "minus")
-   (--> (in-hole E (times (lit number_1) (lit number_2)))
-        (in-hole E (lit ,(* (term number_1) (term number_2))))             "times")
-   (--> (in-hole E (fix (lam x e))) (in-hole E (subst x (fix (lam x e)) e)) "fix")
-   (--> (in-hole E (cons v_1 v_2))  (in-hole E (pair v_1 v_2))            "cons")
-   (--> (in-hole E (if (code e_0) (code e_1) (code e_2)))
-        (in-hole E (reflect (code (if (code e_0) e_1 e_2))))              "ifccc")
-   (--> (in-hole E (if (code e_0) e_1 e_2))
-        (in-hole E (if (code e_0) (reify e_1) (reify e_2)))               "ifc"
+   (--> (in-hole M (plus (lit number_1) (lit number_2)))
+        (in-hole M (lit ,(+ (term number_1) (term number_2))))             "plus")
+   (--> (in-hole M (minus (lit number_1) (lit number_2)))
+        (in-hole M (lit ,(- (term number_1) (term number_2))))             "minus")
+   (--> (in-hole M (times (lit number_1) (lit number_2)))
+        (in-hole M (lit ,(* (term number_1) (term number_2))))             "times")
+   (--> (in-hole M (fix (lam x e))) (in-hole M (subst x (fix (lam x e)) e)) "fix")
+   (--> (in-hole M (cons v_1 v_2))  (in-hole M (pair v_1 v_2))            "cons")
+   (--> (in-hole M (if (code e_0) (code e_1) (code e_2)))
+        (in-hole M (reflect (code (if (code e_0) e_1 e_2))))              "ifccc")
+   (--> (in-hole M (if (code e_0) e_1 e_2))
+        (in-hole M (if (code e_0) (reify e_1) (reify e_2)))               "ifc"
         (side-condition (and (not-code? (term e_1)) (not-code? (term e_2)))))
-   (--> (in-hole E (b_0 (code e_1) (code e_2)))
-        (in-hole E (reflect (code (b_0 e_1 e_2))))                        "bc")
-   (--> (in-hole E (app (code e_1) (code e_2)))
-        (in-hole E (reflect (code (app e_1 e_2))))                        "appc")
-   (--> (in-hole E (lift (lit number_1)))
-        (in-hole E (code (lit number_1)))                                 "lift-lit")
-   (--> (in-hole E (lift (cons (code e_1) (code e_2))))
-        (in-hole E (reflect (code (cons e_1 e_2))))                       "lift-pair")
-   (--> (in-hole E (lift (code e))) (in-hole E (reflect (lift e)))        "lift-code")
-   (--> (in-hole E (lift (lam x (code e))))
-        (in-hole E (reflect (code (lam x e))))                            "lift-lamc")
-   (--> (in-hole E (lift (lam x e)))
-        (in-hole E (lift (lam x (reify (subst x (code x) e)))))           "lift-lam"
+   (--> (in-hole M (b_0 (code e_1) (code e_2)))
+        (in-hole M (reflect (code (b_0 e_1 e_2))))                        "bc")
+   (--> (in-hole M (app (code e_1) (code e_2)))
+        (in-hole M (reflect (code (app e_1 e_2))))                        "appc")
+   (--> (in-hole M (lift (lit number_1)))
+        (in-hole M (code (lit number_1)))                                 "lift-lit")
+   (--> (in-hole M (lift (cons (code e_1) (code e_2))))
+        (in-hole M (reflect (code (cons e_1 e_2))))                       "lift-pair")
+   (--> (in-hole M (lift (code e))) (in-hole M (reflect (lift e)))        "lift-code")
+   (--> (in-hole M (lift (lam x (code e))))
+        (in-hole M (reflect (code (lam x e))))                            "lift-lamc")
+   (--> (in-hole M (lift (lam x e)))
+        (in-hole M (lift (lam x (reify (subst x (code x) e)))))           "lift-lam"
         (side-condition (and (not-code? (term e)) (no-reify? (term e)))))
-   (--> (in-hole E (run v e)) (in-hole E (run v (reify e)))               "run")
-   (--> (in-hole E (run (code e_1) (code e_2)))
-        (in-hole E (reflect (code (run e_1 e_2))))                        "runcc")
-   (--> (in-hole E (run e_1 (code e_2)))
-        (in-hole E (reify e_2))                                           "runc"
+   (--> (in-hole M (run v e)) (in-hole M (run v (reify e)))               "run")
+   (--> (in-hole M (run (code e_1) (code e_2)))
+        (in-hole M (reflect (code (run e_1 e_2))))                        "runcc")
+   (--> (in-hole M (run e_1 (code e_2)))
+        (in-hole M (reify e_2))                                           "runc"
         (side-condition (not-code? (term e_1))))
-   (--> (in-hole R (reify (in-hole E (reflect (code e)))))
-        (in-hole R (let x_new (code e) (reify (in-hole E x_new))))        "reify-reflect"
+   (--> (in-hole R (reify (in-hole M (reflect (code e)))))
+        (in-hole R (let x_new (code e) (reify (in-hole M x_new))))        "reify-reflect"
         (where x_new ,(variable-not-in (term (R E e)) (term x))))
-   (--> (in-hole R (reify e)) (in-hole R e)                               "reify-id"
-        (side-condition (no-reflect? (term e))))
+   (--> (in-hole R (reify v)) (in-hole R v)                               "reify-id"
+        (side-condition (no-reflect? (term v))))
    ))
 
 (define-metafunction vm
