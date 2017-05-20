076b844fbb2b771d1d79551d972f6a7fd3c5d557
Nada Amin <namin@alum.mit.edu>
2017-05-20 18:08:05 +0200

    it works, cc @TiarkRompf

--- vm.rkt
+++ vm.rkt
@@ -3,17 +3,19 @@
 
 (define-language vm
   (e x (lit number) (lam x e) (cons e e) (code e) (let x e e) (app e e) (if e e e) (b e e) (fix e) (lift e) (run e e)
-     (reflect e) (reify e))
+     (reflect e) (lamc x e) (letc x e e))
   (v (lit number) (lam x e) (cons v v) (code e))
   (b plus minus times)
   (E hole (cons E e) (cons v E) (let x E e) (app E e) (app v E) (if E e e) (b E e) (b v E) (fix E e) (lift E) (run E e) (reflect E))
-  (R hole (reify (in-hole E R)) (reify R) (lift (lam x R)) (if (code e) R e) (if (code e) v R) (run (code e) R))
-  (M (in-hole R (reify E)) E)
-  (x (variable-except lit lam cons let app if plus minus times fix lift run reflect reify pair code)))
+  (R hole (lift (lamc x R)) (if (code e) R e) (if (code e) v R) (run (code e) R) (letc x e R))
+  (M hole
+     (cons M e) (cons v M) (let x M e) (app M e) (app v M) (if M e e) (b M e) (b v M) (fix M e) (lift M) (run M e) (reflect M)
+     (lift (lamc x M)) (if (code e) M e) (if (code e) v M) (run (code e) M) (letc x e M))
+  (P R)
+  (x (variable-except lit lam cons let app if plus minus times fix lift run reflect letc code)))
 
 (define not-code? (lambda (x) (not ((redex-match vm (code e)) x))))
 (define no-reflect? (lambda (x) (not ((redex-match vm (in-hole E (reflect any))) x))))
-(define no-reify? (lambda (x) (not ((redex-match vm (in-hole R (reify any))) x))))
 
 (define red
   (reduction-relation
@@ -21,8 +23,8 @@
    (--> (in-hole M (let x v e))                 (in-hole M (subst x v e)) "let")
    (--> (in-hole M (app (lam x e) v))           (in-hole M (subst x v e)) "lam")
    (--> (in-hole M (if (lit 0) e_1 e_2))        (in-hole M e_2)           "if0")
-   (--> (in-hole M (if (lit number_1) e_1 e_2)) (in-hole M e_1)           "ifn"
-        (side-condition (not (= 0 (term number_1)))))
+   (--> (in-hole M (if (lit number_0) e_1 e_2)) (in-hole M e_1)           "ifn"
+        (side-condition (not (= 0 (term number_0)))))
    (--> (in-hole M (plus (lit number_1) (lit number_2)))
         (in-hole M (lit ,(+ (term number_1) (term number_2))))             "plus")
    (--> (in-hole M (minus (lit number_1) (lit number_2)))
@@ -30,12 +32,8 @@
    (--> (in-hole M (times (lit number_1) (lit number_2)))
         (in-hole M (lit ,(* (term number_1) (term number_2))))             "times")
    (--> (in-hole M (fix (lam x e))) (in-hole M (subst x (fix (lam x e)) e)) "fix")
-   (--> (in-hole M (cons v_1 v_2))  (in-hole M (pair v_1 v_2))            "cons")
    (--> (in-hole M (if (code e_0) (code e_1) (code e_2)))
         (in-hole M (reflect (code (if (code e_0) e_1 e_2))))              "ifccc")
-   (--> (in-hole M (if (code e_0) e_1 e_2))
-        (in-hole M (if (code e_0) (reify e_1) (reify e_2)))               "ifc"
-        (side-condition (and (not-code? (term e_1)) (not-code? (term e_2)))))
    (--> (in-hole M (b_0 (code e_1) (code e_2)))
         (in-hole M (reflect (code (b_0 e_1 e_2))))                        "bc")
    (--> (in-hole M (app (code e_1) (code e_2)))
@@ -43,24 +41,19 @@
    (--> (in-hole M (lift (lit number_1)))
         (in-hole M (code (lit number_1)))                                 "lift-lit")
    (--> (in-hole M (lift (cons (code e_1) (code e_2))))
-        (in-hole M (reflect (code (cons e_1 e_2))))                       "lift-pair")
-   (--> (in-hole M (lift (code e))) (in-hole M (reflect (lift e)))        "lift-code")
-   (--> (in-hole M (lift (lam x (code e))))
+        (in-hole M (reflect (code (cons e_1 e_2))))                       "lift-cons")
+   (--> (in-hole M (lift (code e))) (in-hole M (reflect (code (lift e)))) "lift-code")
+   (--> (in-hole M (lift (lamc x (code e))))
         (in-hole M (reflect (code (lam x e))))                            "lift-lamc")
    (--> (in-hole M (lift (lam x e)))
-        (in-hole M (lift (lam x (reify (subst x (code x) e)))))           "lift-lam"
-        (side-condition (and (not-code? (term e)) (no-reify? (term e)))))
-   (--> (in-hole M (run v e)) (in-hole M (run v (reify e)))               "run")
+        (in-hole M (lift (lamc x (subst x (code x) e))))                  "lift-lam")
    (--> (in-hole M (run (code e_1) (code e_2)))
         (in-hole M (reflect (code (run e_1 e_2))))                        "runcc")
-   (--> (in-hole M (run e_1 (code e_2)))
-        (in-hole M (reify e_2))                                           "runc"
-        (side-condition (not-code? (term e_1))))
-   (--> (in-hole R (reify (in-hole E (reflect (code e)))))
-        (in-hole R (let x_new (code e) (reify (in-hole E x_new))))        "reify-reflect"
+   (--> (in-hole P (in-hole E (reflect (code e))))
+        (in-hole P (letc x_new e (in-hole E (code x_new))))               "reify-reflect"
         (where x_new ,(variable-not-in (term (R E e)) (term x))))
-   (--> (in-hole R (reify v)) (in-hole R v)                               "reify-id"
-        (side-condition (no-reflect? (term v))))
+   (--> (in-hole P (letc x_1 e_1 (code e_2)))
+        (in-hole P (code (let x_1 e_1 e_2)))                              "letc")
    ))
 
 (define-metafunction vm
@@ -98,15 +91,15 @@
   (lambda (l)
     (if (null? l) (display "done\n") (begin (display (car l)) (display "\n") (pp-each (cdr l))))))
 
-(pp-each (acc-trace (term (reify (reflect (code (plus (lit 1) (lit 2))))))))
-(pp-each (acc-trace (term (reify (lift (plus (lit 1) (lit 2)))))))
-(pp-each (acc-trace (term (reify (app (lam x x) (lit 2))))))
-(pp-each (acc-trace (term (reify (lift (app (lam x x) (lit 2)))))))
-(pp-each (acc-trace (term (reify (lift (lam x x))))))
-(pp-each (acc-trace (term (reify (lift (if (lit 0) (lit 1) (plus (lit 1) (lit 2))))))))
-(pp-each (acc-trace (term (reify (if (lit 0) (lift (plus (lit 3) (lit 1))) (plus (lift (lit 1)) (lift (lit 2))))))))
-(pp-each (acc-trace (term (reify (plus (lift (lit 0)) (plus (lift (lit 1)) (lift (plus (lit 2) (lit 3)))))))))
-(pp-each (acc-trace (term (reify (plus (plus (lift (lit 0)) (lift (lit 1))) (plus (lift (lit 2)) (lift (lit 3))))))))
+(pp-each (acc-trace (term (reflect (code (plus (lit 1) (lit 2)))))))
+(pp-each (acc-trace (term (lift (plus (lit 1) (lit 2))))))
+(pp-each (acc-trace (term (app (lam x x) (lit 2)))))
+(pp-each (acc-trace (term (lift (app (lam x x) (lit 2))))))
+(pp-each (acc-trace (term (lift (lam x x)))))
+(pp-each (acc-trace (term (lift (if (lit 0) (lit 1) (plus (lit 1) (lit 2)))))))
+(pp-each (acc-trace (term (if (lit 0) (lift (plus (lit 3) (lit 1))) (plus (lift (lit 1)) (lift (lit 2)))))))
+(pp-each (acc-trace (term (plus (lift (lit 0)) (plus (lift (lit 1)) (lift (plus (lit 2) (lit 3))))))))
+(pp-each (acc-trace (term (plus (plus (lift (lit 0)) (lift (lit 1))) (plus (lift (lit 2)) (lift (lit 3)))))))
 
 
 (pp-each (acc-trace (term (app (app
@@ -131,10 +124,10 @@
                 (lit 1)))))
   (lit 6)))))
 
-(pp-each (acc-trace (term (reify (lift (lam x (lift (plus (lit 1) (lit 2)))))))))
+(pp-each (acc-trace (term (lift (lam x (lift (plus (lit 1) (lit 2))))))))
 
-(pp-each (acc-trace (term (reify (lift (lam x (plus x (lift (plus (lit 1) (lit 2))))))))))
+(pp-each (acc-trace (term (lift (lam x (plus x (lift (plus (lit 1) (lit 2)))))))))
 
-(pp-each (acc-trace (term (reify (app (lift (lam x (lift (plus (lit 1) (lit 2))))) (lift (lit 2)))))))
+(pp-each (acc-trace (term (app (lift (lam x (lift (plus (lit 1) (lit 2))))) (lift (lit 2))))))
 
-(pp-each (acc-trace (term (reify (plus (lift (if (lit 0) (lit 1) (lit 2))) (if (lift (lit 0)) (lift (lit 1)) (lift (lit 2))))))))
+(pp-each (acc-trace (term (plus (lift (if (lit 0) (lit 1) (lit 2))) (if (lift (lit 0)) (lift (lit 1)) (lift (lit 2)))))))
