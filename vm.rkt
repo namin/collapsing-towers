1f92835aed057731b36ef901858c86cf11866838
Nada Amin <amin@st.informatik.tu-darmstadt.de>
2017-05-22 16:13:31 +0200

    more experiments

--- vm.rkt
+++ vm.rkt
@@ -172,3 +172,12 @@
 (pp-each (acc-trace (term (app
  (let x4 (lam fac (let x3 (lam n1 (let x2 (if n1 (let x (minus n1 (lit 1)) (let x1 (app fac x) (let x (times n1 x1) x))) (lit 1)) x2)) x3)) (let x (fix x4) x))
  (lit 6)))))
+
+(define fac
+  `(fix (app l (lam fac (app l (lam n
+   (if n
+       (times n (app fac (minus n (app l (lit 1)))))
+       (app l (lit 1)))))))))
+
+(pp-each (acc-trace (term (app (let l (lam x x) ,fac) (lit 6)))))
+(pp-each (acc-trace (second (last (acc-trace (term (app (let l (lam x (lift x)) ,fac) (lift (lit 6)))))))))
