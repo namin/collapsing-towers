649dbc78dce4d1290bf1c273e14f0c3abf76510a
Nada Amin <namin@alum.mit.edu>
2017-06-14 19:55:48 +0200

    put call/cc in, sigh we need mutation to do amb, to be continued...

--- pink.scala
+++ pink.scala
@@ -188,10 +188,11 @@ object Pink_macro extends PinkBase {
     (if (eq?  'cdr    (car exp))   ((((eval l) (cadr exp)) env) (lambda _ v (k (cdr v))))
     (if (eq?  'cons   (car exp))   ((((eval l) (cadr exp)) env) (lambda _ a ((((eval l) (caddr exp)) env) (lambda _ d (k (l (cons a d)))))))
     (if (eq?  'quote  (car exp))   (k (l (cadr exp)))
+    (if (eq? 'call/cc (car exp))   (((((eval l) (cadr exp)) env) (lambda _ p (p (l (lambda _ v (l (lambda _ k1 (k v))))))))  (l (lambda _ v (k v))))
 
 (app 0)
 
-))))))))))))))))
+)))))))))))))))))
 
 (app 0)
 
