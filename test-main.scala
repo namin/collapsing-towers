acbdde2a49beb02591ae27f59ff7ec94a705471e
Nada Amin <namin@alum.mit.edu>
2017-06-19 19:45:50 +0200

    refactor test-main, so it is easier to comment sub parts in and out

--- test-main.scala
+++ test-main.scala
@@ -2,37 +2,49 @@ object TestMain {
   import Base._
   import Lisp._
   import Matches._
+
   def main(args: Array[String]) {
+    //testBase()
+    //testLisp()
+    testPink()
+    //benchmarks()
+  }
+
+  def benchmarks() {
+    println("PINK BENCHMARKS")
+    benchFac()
+  }
+
+  def testBase() {
     testBasic()
     testAck1()
     testAck2()
-
     testFac1()
+  }
+
+  def testLisp() {
     testEval()
     testEvalCps()
     testEvalAmb()
     testDeltaEnv()
     testDelta()
-    //testMutEval()
-    //testMutInEval()
+
+    testMutEval()
+    testMutInEval()
+
     testEvalSyntax()
 
     testMatches()
     testMatchesBis()
 
-    //testUnstaging() // experimental
+    testUnstaging() // experimental
+  }
 
-    //println("done with tests")
-    //println("")
-    //println("PINK BENCHMARKS")
-    //benchFac()
-    //TODO:benchMatches()
-
+  def testPink() {
     Pink.test()
     Pink_clambda.test()
     Pink_CPS.test()
     // disable Pink_macro by default because they requrie stack
     //Pink_macro.test()
   }
-
 }
