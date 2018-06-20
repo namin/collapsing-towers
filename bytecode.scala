616d4850cc2d0263ca56b4be98879f7d54f46f4d
Tiark Rompf <tiark.rompf@gmail.com>
2018-06-20 17:09:38 -0400

    add labels and jumps

--- bytecode.scala
+++ bytecode.scala
@@ -21,12 +21,25 @@ object Bytecode {
 
     run(s"""
     (let ex $exec_src
-    (let prog (lambda _ _ (quote $test_src1));; ignore label atm
+    (let prog (quote $test_src1)
     (let pc 'L0
     (let mem 'nil
     (let sp 1000
-    (((((ex ex) prog) pc) mem) sp))))))
+    ((((ex prog) pc) mem) sp))))))
     """)
+    // expect 5
+
+    run(s"""
+    (let ex $exec_src
+    (let prog (quote $test_src2)
+    (let pc 'L0
+    (let mem 'nil
+    (let sp 1000
+    ((((ex prog) pc) mem) sp))))))
+    """)
+    // expect 5
+
+
 
   }
 
@@ -41,66 +54,57 @@ object Bytecode {
 */
 
 
-val exec_src = commonReplace("""
-(lambda _ exec (lambda _ prog (lambda _ pc (lambda _ mem (lambda _ sp
-  (let update (lambda _ fun (lambda _ i (lambda _ x
-    (lambda _ j (if (eq? i j) x (fun j))))))
-  ;; e.g. ((cst 0) (+) (jmp L1))
-  (let loop (lambda loop exp (lambda _ mem (lambda _ sp
-    (if (eq?  'cst    (caar exp))    (((loop (cdr exp)) (((update mem) sp) (cadar exp))) (+ sp 1))
-    (if (eq?  '+      (caar exp))    (((loop (cdr exp)) (((update mem) (- sp 2)) (+ (mem (- sp 2)) (mem (- sp 1))))) (- sp 1))
-    (if (eq?  'halt   (caar exp))    (mem (- sp 1))
-    'eof))))))
-  (((loop (prog pc)) mem) sp))))))))
-""")
-
-/*
-  (if (num?                exp)    (maybe-lift exp)
-  (if (sym?                exp)    (env exp)
-  (if (sym?           (car exp))   
-    (if (eq?  '+      (car exp))   (+   ((eval (cadr exp)) env) ((eval (caddr exp)) env))
-    (if (eq?  '-      (car exp))   (-   ((eval (cadr exp)) env) ((eval (caddr exp)) env))
-    (if (eq?  '*      (car exp))   (*   ((eval (cadr exp)) env) ((eval (caddr exp)) env))
-    (if (eq?  'eq?    (car exp))   (eq? ((eval (cadr exp)) env) ((eval (caddr exp)) env))
-    (if (eq?  'if     (car exp))   (if  ((eval (cadr exp)) env) ((eval (caddr exp)) env) ((eval (cadddr exp)) env))
-    (if (eq?  'lambda (car exp))   (maybe-lift (lambda f x ((eval (cadddr exp)) 
-      (lambda _ y (if (eq? y (cadr exp)) f (if (eq? y (caddr exp)) x (env y)))))))
-    (if (eq?  'let    (car exp))   (let x ((eval (caddr exp)) env) ((eval (cadddr exp))
-      (lambda _ y (if (eq?  y (cadr exp)) x (env y)))))
-    (if (eq?  'lift   (car exp))   (lift ((eval (cadr exp)) env))
-    (if (eq?  'num?   (car exp))   (num? ((eval (cadr exp)) env))
-    (if (eq?  'sym?   (car exp))   (sym? ((eval (cadr exp)) env))
-    (if (eq?  'car    (car exp))   (car  ((eval (cadr exp)) env))
-    (if (eq?  'cdr    (car exp))   (cdr  ((eval (cadr exp)) env))
-    (if (eq?  'cons   (car exp))   (maybe-lift (cons ((eval (cadr exp)) env) ((eval (caddr exp)) env)))
-    (if (eq?  'quote  (car exp))   (maybe-lift (cadr exp))
-    (if (eq?  'pair?  (car exp))   (pair? ((eval (cadr exp)) env))
-    (if (eq?  'code?  (car exp))   (code? ((eval (cadr exp)) env) ((eval (caddr exp)) env))
-    (if (equs 'refNew (car exp))      (maybe-lift (refNew ((eval (cadr exp)) env)))
-    (if (equs 'refRead (car exp))     (refRead ((eval (cadr exp)) env))
-    (if (equs 'refWrite (car exp))    (refWrite ((eval (cadr exp)) env) ((eval (caddr exp)) env))
-    ((env (car exp)) ((eval (cadr exp)) env)))))))))))))))))))))
-  (((eval (car exp)) env) ((eval (cadr exp)) env)))))))))
-""")
-*/
-
 val test_src1 = commonReplace("""
 (
-  (cst 2)
-  (cst 3)
-  (+)
-  (halt)
+  (L0 (
+    (cst 2)
+    (cst 3)
+    (+)
+    (halt)))
 )
 """)
 
 val test_src2 = commonReplace("""
-(L0 (
-  (cst 2)
-  (cst 3)
-  (+)
-  (halt)
-))
+(
+  (L0 (
+    (cst 2)
+    (cst 3)
+    (jmp L1)))
+  (L1 (
+    (+)
+    (halt)))
+)
 """)
+
+val exec_src = commonReplace("""
+(lambda exc prog (lambda _ pc (lambda _ mem (lambda _ sp
+  ;; generic list lookup: find (eq? a) (a b) = a
+  (let find (lambda find p (lambda _ xs
+    (if (pair? xs)
+      (if (p (car xs)) (car xs) ((find p) (cdr xs)))
+      'nil)))
+  ;; find block in program: find-block 'L0 '((L0 as) (L1 bs)) = as
+  (let find-block (lambda _ prog (lambda _ label
+    (car (cdr ((find (lambda _ lb (eq? label (car lb)))) prog)))))
+  ;; generic function update: fun[i -> x]
+  (let update (lambda _ fun (lambda _ i (lambda _ x
+    (lambda _ j (if (eq? i j) x (fun j))))))
+  ;; exec basic block, e.g. ((cst 0) (+) (jmp L1))
+  (let loop (lambda loop block (lambda _ mem (lambda _ sp
+    (let exp (car block)
+    (if (eq?  'cst    (car exp))    (((loop (cdr block)) (((update mem) sp) (cadr exp))) (+ sp 1))
+    (if (eq?  '+      (car exp))    (((loop (cdr block)) (((update mem) (- sp 2)) (+ (mem (- sp 2)) (mem (- sp 1))))) (- sp 1))
+    (if (eq?  'halt   (car exp))    (mem (- sp 1))
+    (if (eq?  'jmp    (car exp))    ((((exc prog) (cadr exp)) mem) sp)
+    'eob)))))))) ;; failure, end of block
+  ;;((find-block prog) pc)
+  (((loop ((find-block prog) pc)) mem) sp)
+  ))))))))
+""")
+
+
+
+
 
 
 
@@ -130,20 +134,6 @@ L2:
   (halt)
 */
 
-
-
-  val dec_src = """
-  while (i < n) {
-    i = i + 1
-  }
-  return i
-  """
-
-
-
-  val fac_src = """
-  ()
-  """
 
   def testFacBC(): Unit = {
     
